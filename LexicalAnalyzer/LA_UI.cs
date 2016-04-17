b741a5d1bdccb8540a40795540225a538bf0f0bd
uestcNaldo <1165648703@qq.com>
2016-04-17 10:34:07 +0800

    First Commit

---
+++ LexicalAnalyzer/LA_UI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LexicalAnalyzer {
+    public partial class LA_UI : Form {
+        public LA_UI() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
+        
+    }
+}
